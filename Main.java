f7ab8d425d1ddfed7683f6b172e6a940342252a7
Luis-Medina12 <luis.medina0312@gmail.com>
2023-02-15 22:41:37 -0700

    Cleaned up code and added more tests to show speed increase

--- Main.java
+++ Main.java
@@ -1,34 +1,54 @@
 public class Main {
         public static void main(String[] args)
         {
-            // create the Saurian object using the default constructor
-            // this will also create an object of my HashMaps for the translations
-            Saurian saur = new Saurian();
-            String testText = "If you're looking for random paragraphs, you've come to the right place. When a " +
-                    "random word or a random sentence isn't quite enough, the next logical step is to find a " +
-                    "random paragraph. We created the Random Paragraph Generator with you in mind. The " +
-                    "process is quite simple. Choose the number of random paragraphs you'd like to see and " +
-                    "click the button. Your chosen number of paragraphs will instantly appear.";
+                // create the Saurian object using the default constructor
+                // this will also create an object of my HashMaps for the translations
+                Saurian saurHash = new Saurian();
+                SaurianOriginal saurOrig = new SaurianOriginal();
+                String testText = "If you're looking for random paragraphs, you've come to the right place. When a " +
+                        "random word or a random sentence isn't quite enough, the next logical step is to find a " +
+                        "random paragraph. We created the Random Paragraph Generator with you in mind. The " +
+                        "process is quite simple. Choose the number of random paragraphs you'd like to see and " +
+                        "click the button. Your chosen number of paragraphs will instantly appear.";
 
-            System.out.println();
-            System.out.println("Original: "+ testText);
-            System.out.println();
-            System.out.print("Translation: ");
-            // start the clock
-            long startTime = System.nanoTime();
 
-            saur.setEnglish(testText);
-            System.out.print(saur.getSaurian());
+                for(int i =0; i< 10; i++)
+                {                
+                        if(i>0){
+                                testText = testText+ " " + testText;
+                        }
 
-            long endTime = System.nanoTime();
-            // end the clock
+                        // start the clock
+                        long startTimeH = System.nanoTime();
 
-            // The original code takes about 550μs to translate this paragraph.
-            // The new code using HashMaps takes about 130μs to translate the same paragraph.
+                        saurHash.setEnglish(testText);
 
-            System.out.println();
-            System.out.println();
-            System.out.println("*** The translation took " + ((endTime/1000) - (startTime/1000)) + " microseconds. ***");
-            System.out.println();
+                        long endTimeH = System.nanoTime();
+                        // end the clock
+
+                        saurHash.setEnglish(testText);
+
+                        // start the clock
+                        long startTimeO = System.nanoTime();
+
+                        saurOrig.setEnglish(testText);
+
+                        long endTimeO = System.nanoTime();
+
+                        // The original code takes about 550 microseconds to translate this paragraph.
+                        // The new code using HashMaps takes about 130 microseconds to translate the same paragraph.
+
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("*** The translation took " + ((endTimeH/1000) - (startTimeH/1000)) + " microseconds using HashMaps for " +(i+1)+ " paragraphs. ***");
+                        
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("*** The translation took " + ((endTimeO/1000) - (startTimeO/1000)) + " microseconds using Original program for " +(i+1)+ " paragraphs. ***");
+                        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------");
+                }
+                
+                
+                System.out.println();
         }
 }
